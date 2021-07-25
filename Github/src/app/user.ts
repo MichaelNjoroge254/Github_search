b03c671e4b4e0d8001e05f429edbc94b569e12d6
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-07-25 22:23:31 +0300

    add src/app/user.ts

--- Github/src/app/user.ts
+++ Github/src/app/user.ts
@@ -1,2 +1,16 @@
 export class User {
+
+  constructor(
+    public avatar_url: any,
+    public name: any,
+    public login: any,
+    public bio: any,
+    public public_repos: any,
+    public followers: any,
+    public following: any,
+    public location: any,
+    public email: any,
+    public html_url: any,
+    public created_at: any
+  ) { }
 }
