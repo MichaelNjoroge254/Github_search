fc45dc596169c557f31770ead33e22563a3b9e34
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-07-25 22:08:17 +0300

    initial commit

---
+++ Github/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormComponent } from './form/form.component'
+import { FormsModule } from '@angular/forms';
+import { RoutingModule } from './routing/routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ReposComponent } from './repos/repos.component';
+import { RepoFormComponent } from './repo-form/repo-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DayTimePipe } from './day-time.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    FormComponent,
+    ReposComponent,
+    RepoFormComponent,
+    FooterComponent,
+    NotFoundComponent,
+    DayTimePipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Github/src/app/day-time.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { DayTime.Pipe } from './day-time.pipe';
+
+describe('DayTime.Pipe', () => {
+  it('should create an instance', () => {
+    expect(new DayTime.Pipe()).toBeTruthy();
+  });
+});
