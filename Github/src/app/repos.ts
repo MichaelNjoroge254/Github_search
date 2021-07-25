6fc1ea6a168debb460510e2147a769df0f213f00
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-07-25 22:25:00 +0300

    add src/app/repos.ts

--- Github/src/app/repos.ts
+++ Github/src/app/repos.ts
@@ -1,2 +1,11 @@
 export class Repos {
+    constructor(
+        public name: string,
+        public description: string,
+        public html_url: string,
+        public forks: number,
+        public watchers_count: number,
+        public language: string,
+        public created_at: Date
+      ) {}
 }
