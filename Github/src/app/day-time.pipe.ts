0c84e5f2848d2bdf1091539a71c38ec4003ba064
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-07-25 22:15:08 +0300

    add src/app/day-time.pipe.ts

--- Github/src/app/day-time.pipe.ts
+++ Github/src/app/day-time.pipe.ts
@@ -1,2 +1,12 @@
-export class DayTime.Pipe {
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dayTime'
+})
+export class DayTimePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
 }
