ce9eb150e3b17d95178cf1d13e556c33718f1d0a
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-07-26 06:08:11 +0300

    add src/assets/icons8.png

--- Github/src/app/app.module.ts
+++ Github/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatCardModule } from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormComponent } from './form/form.component'
+import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 import { RoutingModule } from './routing/routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -19,7 +19,6 @@ import { RepoFormComponent } from './repo-form/repo-form.component';
 import { FooterComponent } from './footer/footer.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DayTimePipe } from './day-time.pipe';
-import { RoutingComponent } from './routing/routing.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +30,6 @@ import { RoutingComponent } from './routing/routing.component';
     FooterComponent,
     NotFoundComponent,
     DayTimePipe,
-    RoutingComponent,
   ],
   imports: [
     BrowserModule,
