e7d258d04b36722f12e93192d384dd881cc99ea9
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-08-11 22:37:01 +0300

    initial commit

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,18 +1,19 @@
 export class User {
+    constructor(
+        public avatar_url: string,
+        public name: string,
+        public bio: string,
+        public login :string,
+        public company: string,
+        public location: string,
+        public followers: number,
+        public following: number,
+        public repos_url:string,
+        public searchTerm:string,
+        public public_repos:string,
 
-  constructor(
-    public avatar_url: any,
-    public name: any,
-    public login: any,
-    public bio: any,
-    public public_repos: any,
-    public followers: any,
-    public following: any,
-    public location: any,
-    public email: any,
-    public html_url: any,
-    public created_at: any
-  ) {
+    ){
+
+    }
     
-   }
 }
